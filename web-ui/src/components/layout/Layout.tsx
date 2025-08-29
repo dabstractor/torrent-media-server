595f7c8ec8f7702efaf3bf01b76fb3fc9587b644
Dustin Schultz <dustindschultz@gmail.com>
2025-08-28 22:22:01 -0400

    fixed nav links

--- web-ui/src/components/layout/Layout.tsx
+++ web-ui/src/components/layout/Layout.tsx
@@ -25,7 +25,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <Header onToggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
       <div className="flex flex-1 relative">
         <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
-        <main className="flex-1 p-4 sm:p-6 md:p-8">{children}</main>
+        <main className="flex-1 p-4 sm:p-6 md:p-8 w-full">{children}</main>
       </div>
       <Footer />
     </div>
